39b4ed1da7a91d0a4c2aed665d33d8a2f627193c
Milan Keca <vonum.mk@gmail.com>
2018-03-17 19:11:40 +0100

    Install jsx

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.0"}
+      {:httpoison, "~> 1.0"},
+      {:jsx, "~> 2.0"}
     ]
   end
 end
